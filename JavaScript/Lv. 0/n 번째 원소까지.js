ad3a98c9de7f3572e2b334a5ff048548252af958
Observant0120 <vjw21@naver.com>
2023-06-09 23:38:46 +0900

    feat: Lv.0 추가 / 문자열의 앞의 n글자, 소문자로 바꾸기, n 번째 원소까지

---
+++ JavaScript/Lv. 0/n 번째 원소까지.js
@@ -0,0 +1,5 @@
+function solution(num_list, n) {
+  return num_list.slice(0, n);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181889
---
+++ JavaScript/Lv. 0/문자열의 앞의 n글자.js
@@ -0,0 +1,5 @@
+function solution(my_string, n) {
+  return my_string.slice(0, n);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181907
