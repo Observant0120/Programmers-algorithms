d6c87b280b5340ce967b9f4bc6b0712d3e958e67
Observant0120 <vjw21@naver.com>
2023-06-12 23:36:40 +0900

    feat: Lv.0 추가 / 문자열로 변환, 문자열을 정수로 변환하기, 배열 자르기

---
+++ JavaScript/Lv. 0/문자열로 변환.js
@@ -0,0 +1,5 @@
+function solution(n) {
+  return "" + n;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181845
---
+++ JavaScript/Lv. 0/문자열을 정수로 변환하기.js
@@ -0,0 +1,5 @@
+function solution(n_str) {
+  return parseInt(n_str);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181848
