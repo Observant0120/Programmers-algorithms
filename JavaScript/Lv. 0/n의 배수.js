1f409dfaf2ef0c80ad3bb1f87ebc02381944132c
Observant0120 <vjw21@naver.com>
2023-06-13 23:07:07 +0900

    feat: Lv.0 추가 / 공배수, 배열 두 배 만들기, n의 배수

---
+++ JavaScript/Lv. 0/n의 배수.js
@@ -0,0 +1,5 @@
+function solution(num, n) {
+  return num % n === 0 ? 1 : 0;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181937
---
+++ JavaScript/Lv. 0/배열 두 배 만들기.js
@@ -0,0 +1,5 @@
+function solution(numbers) {
+  return numbers.map((el) => el * 2);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809
