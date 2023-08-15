837b86c83fe33b222d0cd402a8b262314d626dca
Observant0120 <vjw21@naver.com>
2023-08-15 16:24:01 +0900

    feat: Lv.1 추가 / x만큼 간격이 있는 n개의 숫자

---
+++ JavaScript/Lv. 1/x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,11 @@
+function solution(x, n) {
+  let result = [];
+
+  for (let i = 1; i <= n; i++) {
+    result.push(x * i);
+  }
+
+  return result;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12954
